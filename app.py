e9579229385678389186072494b81d298000a137
Arnav Shah <arnavshah30@gmail.com>
2020-02-19 14:45:12 -0500

    Added basic register route

--- app.py
+++ app.py
@@ -17,5 +17,9 @@ conn = pymysql.connect(host='localhost',
 def index():
 	return render_template('index.html')
 
+@app.route('/register')
+def register():
+	return render_template('register.html')
+
 if __name__ == '__main__':
 	app.run('127.0.0.1', 5000, debug=True)

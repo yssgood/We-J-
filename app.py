ef29fb42db1c8a4f7222fee87c6740166d991fce
Sunny Yoo <ssy289@nyu.edu>
2020-02-19 16:29:00 -0500

    Updated IndexPage

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import pymysql.cursors
 
-from modules.user import User
+from modules.User import User
 
 # Initialize Flask
 app = Flask(__name__)

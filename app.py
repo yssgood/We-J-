9ae4272fab4bf378aead1336194bbf6507617674
Arnav Shah <arnavshah30@gmail.com>
2020-02-19 15:27:44 -0500

    Fixed register route args

--- app.py
+++ app.py
@@ -31,11 +31,9 @@ def registerAuth():
 	username = request.form['username']
 	password = request.form['password']
 	newUser = User(email)
-	newUser.insertRegisterDetails(conn, email, username, password)
+	newUser.insertRegisterDetails(conn, username, password)
 	activeUsers.append(newUser)
-	print(activeUsers)
 	return render_template('index.html')
 
 if __name__ == '__main__':
-	#app.run('127.0.0.1', 5000, debug=True)
-	app.run(host='0.0.0.0', port=80)
+	app.run('127.0.0.1', 5000, debug=True)
--- modules/User.py
+++ modules/User.py
@@ -4,9 +4,9 @@ class User:
 	def __init__(self, email):
 		self.email = email
 
-	def insertRegisterDetails(self, conn, email, username, password):
+	def insertRegisterDetails(self, conn, username, password):
 		cursor = conn.cursor()
 		query = 'INSERT INTO User(email, username, password) VALUES(%s, %s, %s)'
-		cursor.execute(query, (email, username, password))
+		cursor.execute(query, (self.email, username, password))
 		conn.commit()
 		cursor.close()

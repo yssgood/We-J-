f76c5d64b80de0893fda69c29e18d6feb92d2341
Sunny Yoo <ssy289@nyu.edu>
2020-02-26 15:57:58 -0500

    added group schema

--- database-schema.sql
+++ database-schema.sql
@@ -8,4 +8,10 @@ CREATE TABLE User(
     email VARCHAR(25),
     password CHAR(64),
     PRIMARY KEY (email)
+
+DROP TABLE IF EXISTS 'MusicGroup';
+CREATE TABLE MusicGroup(
+	email VARCHAR(25), 
+    groupName VARCHAR(25), 
+    PRIMARY KEY (email, groupName)
 );

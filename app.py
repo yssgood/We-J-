f7442363419e5aaba6bb441953ed9cc1464cf71e
Arnav Shah <35004996+arnavshah30@users.noreply.github.com>
2020-02-27 11:02:06 -0500

    Changed 'user' to 'User'

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, session, redirect
 import pymysql.cursors
 
-from modules.user import user
+from modules.user import User
 from modules.group import Group
 
 # Initialize Flask
